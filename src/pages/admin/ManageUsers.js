7856bc1581359678d217a4e00d955a390ecd7f76
Show-Key <127041780+Show-Keys@users.noreply.github.com>
2025-05-18 07:59:12 +0400

    cc

--- src/pages/admin/ManageUsers.js
+++ src/pages/admin/ManageUsers.js
@@ -72,11 +72,11 @@ const ManageUsers = () => {
                 <i className="fas fa-gavel"></i> Add Auctions
               </a>
             </li>
-            <li>
+            {/* <li>
               <a href="/ManageBids">
                 <i className="fas fa-money-bill-wave"></i> Manage Bids
               </a>
-            </li>
+            </li> */}
 
           </ul>
         </nav>
